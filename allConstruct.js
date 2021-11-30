3e9a520c40b9e547aab8ea68d5f98e4abcff4582
gyansatapathy <gyan.satapathy@gmail.com>
2021-11-30 12:13:50 +0530

    1 file modified

--- allConstruct.js
+++ allConstruct.js
@@ -3,7 +3,7 @@
  *
  */
 
-const allConstruct = (target, words) => {
+const allConstruct = (target, words, memo) => {
   // console.log('memo' + JSON.stringify(memo));
   if (target in memo) return memo[target];
   if (target === '') return [[]];
@@ -23,14 +23,14 @@ const allConstruct = (target, words) => {
 
 console.log(allConstruct('wayne', ['aw', 'yn', 'e']));
 console.log(allConstruct('abcdef', ['f', 'ab', 'c', 'e', 'd']));
-// console.log(
-//   allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
-//     'e',
-//     'ee',
-//     'eee',
-//     'eeee',
-//     'eeeee',
-//     'eeeeee',
-//   ])
-// );
+console.log(
+  allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
+    'e',
+    'ee',
+    'eee',
+    'eeee',
+    'eeeee',
+    'eeeeee',
+  ])
+);
 console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
