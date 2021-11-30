8ad0da074d8d2d53f71a1439e6e25b785c7b9d40
gyansatapathy <gyan.satapathy@gmail.com>
2021-11-30 12:10:50 +0530

    1 file modified

--- allConstruct.js
+++ allConstruct.js
@@ -5,31 +5,32 @@
 
 const allConstruct = (target, words) => {
   // console.log('memo' + JSON.stringify(memo));
-  // if (target in memo) return memo[target];
+  if (target in memo) return memo[target];
   if (target === '') return [[]];
 
   let result = [];
   for (let word of words) {
     if (target.indexOf(word) === 0) {
       const suffix = target.substring(word.length);
-      const subResult = allConstruct(suffix, words);
+      const subResult = allConstruct(suffix, words, memo);
       const newResult = subResult.map((res) => [word, ...res]);
       result.push(...newResult);
     }
   }
+  memo[target] = result;
   return result;
 };
 
 console.log(allConstruct('wayne', ['aw', 'yn', 'e']));
 console.log(allConstruct('abcdef', ['f', 'ab', 'c', 'e', 'd']));
-console.log(
-  allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
-    'e',
-    'ee',
-    'eee',
-    'eeee',
-    'eeeee',
-    'eeeeee',
-  ])
-);
+// console.log(
+//   allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
+//     'e',
+//     'ee',
+//     'eee',
+//     'eeee',
+//     'eeeee',
+//     'eeeeee',
+//   ])
+// );
 console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
