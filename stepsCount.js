1f3ecb5bb517b877c8e04f590fa16bfee060b35c
Gyan <gyan.satapathy@gmail.com>
2021-12-02 16:01:14 +0530

    Implemented climb stairs

--- stepsCount.js
+++ stepsCount.js
@@ -1,21 +1,16 @@
-const climbStairs = function (n) {
-    if (n < 0) {
-        return 0;
-    }
+const climbStairs = function (n, memo={}) {
+    if(n in memo) return memo[n];
     if (n === 1) {
         return 1;
     }
     if (n === 2) {
         return 2;
     }
-
-
-    const sum1 = climbStairs(n - 1)
-    const sum2 = climbStairs(n - 2)
-    console.log(sum1);
-    console.log(sum1)
+    const sum1 = climbStairs(n - 1);
+    const sum2 = climbStairs(n - 2);
+    memo[n] = sum1+sum2;
     return sum1 + sum2;
 
 };
 
-console.log(climbStairs(1));
+console.log(climbStairs(6));
