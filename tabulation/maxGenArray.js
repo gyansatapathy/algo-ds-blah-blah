af0a490bb7ece9396dc5d690158bfef055db49d2
Gyan <gyan.satapathy@gmail.com>
2021-12-05 11:49:42 +0530

    Implemented maxGenArray.js

---
+++ tabulation/maxGenArray.js
@@ -0,0 +1,37 @@
+/**
+ * leetcode problem: https://leetcode.com/problems/get-maximum-in-generated-array/
+ * Time complexity O(n)
+ * Space Complexity O(1)
+ */
+const getMaximumGenerated = function (n) {
+
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+
+    const table = Array(n + 1);
+    table[0] = 0;
+    table[1] = 1;
+    let max = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let offset = i % 2;
+
+        if (offset === 0) {
+            table[i] = table[i / 2];
+        } else {
+            const index = (i - 1) / 2;
+            table[i] = table[index] + table[index + 1];
+        }
+
+        max = Math.max(table[i], max);
+    }
+
+    return max;
+};
+
+console.log(getMaximumGenerated(7));
+console.log(getMaximumGenerated(10000));
